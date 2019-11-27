d9d9507020c89583965f39b528a541e252ed1d0e
southernwestern <solomoncblake@gmail.com>
2019-11-27 01:49:34 +0000

    Implemented automated DDL

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS host_agent;
+CREATE DATABASE host_agent;
+\c host_agent;
+
+DROP TABLE IF EXISTS PUBLIC.host_infO;
+CREATE TABLE PUBLIC.host_info
+(
+	id               SERIAL NOT NULL, 
+	hostname         VARCHAR NOT NULL UNIQUE, 
+	cpu_number       INT NOT NULL,
+	cpu_architecture VARCHAR(6) NOT NULL,
+	cpu_model        VARCHAR NOT NULL,
+	cpu_mhz          REAL NOT NULL,
+	L2_cache         INT NOT NULL,
+	total_mem        INT NOT NULL,
+	"timestamp"       TIMESTAMP NOT NULL,
+	PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS PUBLIC.host_usage;
+CREATE TABLE PUBLIC.host_usage 
+( 
+	"timestamp"    TIMESTAMP NOT NULL, 
+	host_id  SERIAL NOT NULL REFERENCES host_info(id),
+	memory_free    INT NOT NULL,
+	cpu_idel      REAL NOT NULL,
+	cpu_kernel     REAL NOT NULL,
+	disk_io        INT NOT NULL,
+	disk_available INT NOT NULL, 
+	PRIMARY KEY("timestamp", host_id)
+); 
